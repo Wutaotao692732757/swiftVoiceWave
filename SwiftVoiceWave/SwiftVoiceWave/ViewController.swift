37012c01fdf7bedbe9d49226baae6967117145e6
wutaotao <wutaotao@wutaotaodeMac-mini.local>
2017-01-09 14:05:27 +0800

    build up

---
+++ SwiftVoiceWave/SwiftVoiceWave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftVoiceWave
+//
+//  Created by wutaotao on 2017/1/9.
+//  Copyright © 2017年 wutaotao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
